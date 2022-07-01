173b8ee95c525c15fd0b71a5f2d75235c6637893
roxannelagazon <103943980+roxannelagazon@users.noreply.github.com>
2022-07-02 04:57:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const userviewRouter = require('./routes/uview.js')
 const session = require('express-session');
 const path = require('path');
 
-const port = 80
+const port = 3000
 const app = express()
  
 //MIDDLEWARE AND VIEWS
